43481da54245f8ebe9c5596c31cf0588f00fca7a
Daniel Perez <daniel@perez.sh>
2020-04-30 17:31:27 +0100

    Improve aave plot

--- aave/analyze_aave.py
+++ aave/analyze_aave.py
@@ -2,8 +2,14 @@ import datetime as dt
 import json
 
 import pandas as pd
+import numpy as np
 import seaborn as sns
+import matplotlib
 import matplotlib.pyplot as plt
+
+
+matplotlib.rc("font", size=20)
+matplotlib.rc("lines", linewidth=2)
 
 
 with open("./aave.jsonl") as f:
@@ -14,13 +20,26 @@ df["datetime"] = pd.to_datetime(df["timestamp"], unit="s")
 
 df["rate"] = df["liquidityRate"].astype(float) / 1e26
 
-df = df[df["datetime"] >= dt.datetime(2020, 4, 1)]
+df["utilization_rate"] = df.utilizationRate.astype(np.float)
 
-currency = df[df["reserve.symbol"] == "SNX"]
+# df = df[df["datetime"] >= dt.datetime(2020, 4, 1)]
+df["Currency"] = df["reserve.symbol"]
+
+highest_variance_currencies = df.groupby(by="Currency")["rate"].std().sort_values(ascending=False).iloc[:5]
+
+highest_variance_currencies.index.values
+to_plot = df[df["reserve.symbol"].isin(highest_variance_currencies.index.values)]
+
+ax = sns.lineplot(x="datetime", y="utilization_rate", hue="Currency", data=to_plot)
+
+# currency = df[df["reserve.symbol"] == "SNX"]
 
 plt.figure(figsize=(15, 10))
-g = sns.lineplot(x="datetime", y="rate", data=currency)
-# g = sns.lineplot(x="datetime", y="rate", hue="reserve.symbol", data=df)
+# ax = sns.lineplot(x="datetime", y="rate", data=currency)
+ax = sns.lineplot(x="datetime", y="rate", hue="Currency", data=to_plot)
+ax.set_xlabel("Date")
+ax.set_ylabel("Interest rate")
 plt.xticks(rotation=45)
 
-plt.savefig("interest-rates-april.png")
+plt.tight_layout()
+plt.savefig("../tmp/interest-rates.pdf")
