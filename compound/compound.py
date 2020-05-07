0408a1a3424e426d5ad3ba8e930901f557762d69
Lewis Gudgeon <ljfgudgeon@gmail.com>
2020-05-08 00:06:54 +0100

    Get high res data for compound

--- compound/compound.py
+++ compound/compound.py
@@ -3,11 +3,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as dates
 import matplotlib.ticker as ticker
+from itertools import tee
+from typing import Iterable, Tuple, TypeVar, List
+
 
 import datetime as dt
 import statsmodels.api as sm
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
+
+T = TypeVar('T')
 
 token_addresses = {'cbat': '0x6c8c6b02e7b2be14d4fa6022dfd6d75921d90e4e',
                     'cdai': '0x5d3a536e4d6dbd6114cc1ead35777bab948e3643',
@@ -23,55 +28,97 @@ variables = ['total_borrows_history', 'total_supply_history', 'utilization_ratio
 
 url = 'https://api.compound.finance/api/v2/market_history/graph'
 
-start_date = int(dt.datetime(2020, 1, 1).timestamp())
-end_date = int(dt.datetime.now().timestamp())
-num_buckets = 3000
+def generate_dates(start: dt.datetime, end: dt.datetime,
+                   buckets_count: int) -> Iterable[dt.datetime]:
+    """Generates a list of ``buckets_count + 1`` dates between
+    ``start`` and ``end``
+
+    :param start: the first date of the sequence to generate
+    :param end: the last date of the sequence to generate
+    :param buckets_count: the number of dates to generate - 1
+    """
+    diff = (end - start) / buckets_count
+    for i in range(buckets_count):
+        yield start + diff * i
+    yield end
+
+
+def pairwise(iterable: Iterable[T]) -> Iterable[Tuple[T, T]]:
+    """Returns an iterable of shifted pairs
+    >>> list(pairwise(['a', 'b', 'c', 'd']))
+    [('a', 'b'), ('b', 'c'), ('c', 'd')]
+    """
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
 
 def format_date(x, pos=None):
      return dates.num2date(x).strftime('%Y-%m-%d')
 
-def make_dataframe(token: str):
+end_date = int(dt.datetime(2020, 7, 5, 0).timestamp())
+start_date = int((dt.datetime(2020, 7, 5, 0) - dt.timedelta(days = 365)).timestamp())
+
+list_dates = list(pairwise(generate_dates(start=start_date, 
+                    end=end_date, buckets_count=365)))
+
+# faulty = [(1588894488.0, 1588980852.0)]
+
+def make_dataframe(token: str, days):
     '''
     Build money-market dataframes. 
     '''
-    params = {'asset': token_addresses[token],
-            'min_block_timestamp': start_date,
-            'max_block_timestamp': end_date,
-            'num_buckets': num_buckets}
+    master_df = pd.DataFrame()
 
-    response = requests.get(url = url, params=params)
-    response = response.json()
+    for day in days:
 
-    rate_variables = ['borrow_rates', 'exchange_rates', 'supply_rates']
+        params = {'asset': token_addresses[token],
+                'min_block_timestamp': int(day[0]),
+                'max_block_timestamp': int(day[1]),
+                'num_buckets': 86400}
 
-    df = pd.DataFrame()
+        response = requests.get(url = url, params=params)
+        response = response.json()
 
-    for key in rate_variables:
-        df_temp = pd.DataFrame(response[key])
-        df_temp['date'] = pd.to_datetime(df_temp['block_timestamp'], unit='s')
-        df_temp.set_index('date', inplace = True)
-        df_temp = df_temp[['rate']]
-        df_temp = df_temp.rename(columns={'rate': str(key)})
-        df = pd.concat([df, df_temp], axis=1)
+        if response['borrow_rates'] == []:
+            continue
 
-    stock_variables = ['total_borrows_history', 'total_supply_history']
+        rate_variables = ['borrow_rates', 'exchange_rates', 'supply_rates']
 
-    for key in stock_variables:
-        df_temp = pd.DataFrame(response[key])
-        df_temp['date'] = pd.to_datetime(df_temp['block_timestamp'], unit='s')
-        df_temp.set_index('date', inplace = True)
-        df_temp = df_temp[['total']]
-        df_temp = pd.concat([df_temp.drop(['total'], axis=1), df_temp['total'].apply(pd.Series)], axis=1)
-        df_temp = df_temp.rename(columns={'value': str(key)})
-        df = pd.concat([df, df_temp], axis=1)
+        df = pd.DataFrame()
 
-    df['total_borrows_history'] = pd.to_numeric(df['total_borrows_history'], downcast="float")
-    df['total_supply_history'] = pd.to_numeric(df['total_supply_history'], downcast="float")
+        for key in rate_variables:
+            df_temp = pd.DataFrame(response[key])
+            df_temp['date'] = pd.to_datetime(df_temp['block_timestamp'], unit='s')
+            df_temp.set_index('date', inplace = True)
+            df_temp = df_temp[['rate']]
+            df_temp = df_temp.rename(columns={'rate': str(key)})
+            df = pd.concat([df, df_temp], axis=1)
 
-    df['utilization_ratio'] = df['total_borrows_history'] / df['total_supply_history']
-    df['spread'] = df['borrow_rates']- df['supply_rates']
+        stock_variables = ['total_borrows_history', 'total_supply_history']
 
-    return df
+        for key in stock_variables:
+            df_temp = pd.DataFrame(response[key])
+            df_temp['date'] = pd.to_datetime(df_temp['block_timestamp'], unit='s')
+            df_temp.set_index('date', inplace = True)
+            df_temp = df_temp[['total']]
+            df_temp = pd.concat([df_temp.drop(['total'], axis=1), df_temp['total'].apply(pd.Series)], axis=1)
+            df_temp = df_temp.rename(columns={'value': str(key)})
+            df = pd.concat([df, df_temp], axis=1)
+
+        df['total_borrows_history'] = pd.to_numeric(df['total_borrows_history'], downcast="float")
+        df['total_supply_history'] = pd.to_numeric(df['total_supply_history'], downcast="float")
+
+        df['utilization_ratio'] = df['total_borrows_history'] / df['total_supply_history']
+        df['spread'] = df['borrow_rates']- df['supply_rates']
+
+        master_df = master_df.append(df)
+
+    return master_df
+
+df.to_pickle('./data/market_history_data.pkl')
+df2 = pd.read_pickle('./data/market_history_data.pkl')
+
+
 
 def plot_utilization_vs_rate(token: str):
     '''
